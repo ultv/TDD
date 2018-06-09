9c43d52d1888634444742386a653d461c3a964f8
Alex <ultv@inbox.ru>
2018-06-09 09:38:24 +0400

    Create OpenBrowser.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TDD
 {
-    partial class Form1
+    partial class FormAvitoCat
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonFindCat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFindCat
+            // 
+            this.buttonFindCat.Location = new System.Drawing.Point(317, 384);
+            this.buttonFindCat.Name = "buttonFindCat";
+            this.buttonFindCat.Size = new System.Drawing.Size(172, 23);
+            this.buttonFindCat.TabIndex = 0;
+            this.buttonFindCat.Text = "Подобрать котенка";
+            this.buttonFindCat.UseVisualStyleBackColor = true;
+            this.buttonFindCat.Click += new System.EventHandler(this.buttonFindCat_Click);
+            // 
+            // FormAvitoCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonFindCat);
+            this.Name = "FormAvitoCat";
+            this.Text = "AvitoCat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormAvitoCat_FormClosing);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonFindCat;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
 
 namespace TDD
 {
-    public partial class Form1 : Form
+    public partial class FormAvitoCat : Form
     {
-        public Form1()
+        IWebDriver browser;
+
+        public FormAvitoCat()
         {
             InitializeComponent();
+        }
+
+        private void buttonFindCat_Click(object sender, EventArgs e)
+        {
+            OpenBrowser("http://avito.ru");
+        }
+
+        public void OpenBrowser(string url)
+        {
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Manage().Window.Maximize();
+            browser.Navigate().GoToUrl(url);
+        }
+
+        private void FormAvitoCat_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            browser.Quit();
         }
     }
 }
