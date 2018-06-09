dd8b61321bf18a373bf9b3272161608932525183
Alex <ultv@inbox.ru>
2018-06-09 10:54:48 +0400

    Create SelectCategory.

--- Form1.cs
+++ Form1.cs
@@ -8,12 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace TDD
 {
     public partial class FormAvitoCat : Form
     {
         IWebDriver browser;
+        IWebElement selectCategory; // ClassName("js-search-form-category ")
+        IWebElement searchButton; // ClassName("search button button-origin")
+        IWebElement catalogBreed; // ClassName("catalog-counts__row clearfix");
 
         public FormAvitoCat()
         {
@@ -23,6 +27,7 @@ namespace TDD
         private void buttonFindCat_Click(object sender, EventArgs e)
         {
             OpenBrowser("http://avito.ru");
+            SelectCategory("Кошки");
         }
 
         public void OpenBrowser(string url)
@@ -30,6 +35,22 @@ namespace TDD
             browser = new OpenQA.Selenium.Chrome.ChromeDriver();
             browser.Manage().Window.Maximize();
             browser.Navigate().GoToUrl(url);
+        }
+
+        public void SelectCategory(string category)
+        {            
+            selectCategory = browser.FindElement(By.ClassName("js-search-form-category "));           
+            SelectElement select = new SelectElement(selectCategory);
+            IList<IWebElement> options = select.Options;
+            select.SelectByText(category);
+            searchButton = browser.FindElement(By.ClassName("button-origin"));
+            searchButton.Click();
+        }
+
+        public void SelectFromCatalog()
+        {
+
+            //IList<IWebElement> catalog = 
         }
 
         private void FormAvitoCat_FormClosing(object sender, FormClosingEventArgs e)
